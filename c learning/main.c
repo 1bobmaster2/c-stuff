c39589b817712dc5cff2168cef33083e8cd1fc50
1bobmaster2 <fkuba556@gmail.com>
2025-03-22 21:43:30 +0100

    hi i love c, cant wait for d

---
+++ c learning/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char yesimsigma[] = "a";
+    int i = 0;
+    
+    while (1 == 1)
+    {
+        printf(yesimsigma);
+        i++;
+    }
+
+    if(i == 100000)
+    {
+        return 0;
+    }
+    
+
+    return 0;
+}

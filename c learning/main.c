f574cb1a1a2c5fa8b16a813fb8250039e2a79a0a
1bobmaster2 <fkuba556@gmail.com>
2025-03-23 10:41:37 +0100

    Update main.c

--- c learning/main.c
+++ c learning/main.c
@@ -1,19 +1,45 @@
 #include <stdio.h>
 
-int main() {
-    char yesimsigma[] = "a";
-    int i = 0;
-    
-    while (1 == 1)
+void ReverseCharArray(char array[], int sizeOfArray)
+{
+    int obieg = 0;
+    int correctSizeOfArray = sizeOfArray - 1;
+    for (size_t i = correctSizeOfArray; i > 0; i--)
     {
-        printf(yesimsigma);
-        i++;
+        array[obieg] = array[correctSizeOfArray];
+        obieg++;
+        correctSizeOfArray--;
     }
 
-    if(i == 100000)
-    {
-        return 0;
-    }
+    printf("%s\n", array);
+    
+}
+
+
+int main() {
+    // char yesimsigma[] = "a";
+    // int i = 0;
+    
+    // while (1 == 1)
+    // {
+    //     printf(yesimsigma);
+    //     i++;
+    // }
+
+    // if(i == 100000)
+    // {
+    //     return 0;
+    // }
+
+    char input[100];
+    int inputSize = 0;
+    printf("input the string to reverse \n");
+    scanf("%s", input);
+    printf("now input the size of the string \n");
+    scanf("%d", &inputSize);
+
+    ReverseCharArray(input, inputSize);
+    
     
 
     return 0;
